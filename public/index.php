a4c5e17fe5b7344a16720c88ddb0f86f4769cf8a
Your Name <you@example.com>
2017-08-05 16:56:19 +0100

    .

--- public/index.php
+++ public/index.php
@@ -1,4 +1,58 @@
 <?php
+if (!file_exists('../config.php')) {
+    file_put_contents('../config.php', "<?php
+/**
+ * Config - Defines stuff!..
+ */
+
+\$config = [
+    // plinker configuration
+    'plinker' => [
+        // self should point to this instance
+        'peer' => 'http://{$_SERVER['HTTP_HOST']}',
+
+        // tracker which seeds peers
+        'tracker' => 'http://{$_SERVER['HTTP_HOST']}',
+
+        // network keys
+        'public_key'  => '".hash('sha256', uniqid(true))."',
+        // required to add nodes
+        'private_key' => '".hash('sha256', uniqid(true))."',
+
+        'enabled' => true,
+        'encrypted' => true
+    ],
+
+    // database connection
+    // default: sqlite:'.__DIR__.'/database.db
+    'database' => [
+        'dsn' => 'sqlite:".realpath(__DIR__."/../")."/database.db',
+        'username' => '',
+        'password' => '',
+        'freeze' => false,
+        'debug' => false
+    ],
+
+    // displays output to consoles
+    'debug' => true,
+
+    // daemon sleep time
+    'sleep_time' => 1,
+
+    // webui login
+    'webui' => [
+        'user' => 'admin',
+        'pass' => 'admin'
+    ]
+];
+
+// define debug error reporting/output
+if (\$config['debug'] === true) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+}");
+}
+
 require '../config.php';
 require '../vendor/autoload.php';
 
@@ -22,7 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         isset($_POST['public_key'])
     ) {
         if ($config['plinker']['enabled'] === false) {
-            exit('Node not active.');
+            exit('Node not enabled.');
         }
 
         // test its encrypted
